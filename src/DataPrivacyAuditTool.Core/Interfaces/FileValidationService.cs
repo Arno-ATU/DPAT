0c6f55fca394773c9e5590ebc6ca573a10fde980
Arno-ATU <190516820+Arno-ATU@users.noreply.github.com>
2025-03-21 14:20:24 +0000

    adding File Validation Service

---
+++ src/DataPrivacyAuditTool.Core/Interfaces/FileValidationService.cs
@@ -0,0 +1,11 @@
+// IFileValidationService.cs
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace DataPrivacyAuditTool.Core.Interfaces
+{
+    public interface IFileValidationService
+    {
+        Task<bool> ValidateFileAsync(IFormFile file);
+    }
+}
